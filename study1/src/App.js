a94200636764d83953d39f21893f90945327255f
Leehaeun0 <a01050675111@gmail.com>
2020-05-07 23:21:46 +0900

    refactoring

--- study1/src/App.js
+++ study1/src/App.js
@@ -33,19 +33,20 @@ class App extends Component {
   };
 
   checkValue = ({ target }) => {
-    return [...target.parentNode.children].some((node) =>
+    return [...target.firstElementChild.children].some((node) =>
       node.nodeName !== 'INPUT' ? false : node.value === ''
     );
   };
 
-  btnClick = (e) => {
+  formSubmit = (e) => {
+    e.preventDefault();
     if (this.checkValue(e)) {
       console.log('입력란을 모두 입력해 주세요');
       return;
     }
     console.log(`아이디: '${this.idValue}', 비밀번호: '${this.passValue}'`);
     console.log('로그인 시도');
-    const [id, pass] = [...e.target.parentNode.children].filter(
+    const [id, pass] = [...e.target.firstElementChild.children].filter(
       (node) => node.nodeName === 'INPUT'
     );
     id.value = '';
@@ -56,29 +57,26 @@ class App extends Component {
 
   render() {
     return (
-      <form action="#" method="post">
+      <form action="#" method="post" onSubmit={this.formSubmit}>
         <fieldset>
-          <legend>
-            <span className={st('label')}>ID</span>
-            <input
-              type="text"
-              id="idInput"
-              onChange={this.inputChange}
-              onBlur={this.inputAssign}
-              onKeyDown={this.inputEnter}
-            />
-            <span className={st('label')}>PASS</span>
-            <input
-              type="text"
-              id="passInput"
-              onChange={this.inputChange}
-              onBlur={this.inputAssign}
-              onKeyUp={this.inputEnter}
-            />
-            <button type="button" onClick={this.btnClick}>
-              로그인
-            </button>
-          </legend>
+          <legend className={st('a11yHidden')}>로그인</legend>
+          <span className={st('label')}>ID</span>
+          <input
+            type="text"
+            id="idInput"
+            onChange={this.inputChange}
+            onBlur={this.inputAssign}
+            onKeyDown={this.inputEnter}
+          />
+          <span className={st('label')}>PASS</span>
+          <input
+            type="password"
+            id="passInput"
+            onChange={this.inputChange}
+            onBlur={this.inputAssign}
+            onKeyUp={this.inputEnter}
+          />
+          <button type="submit">로그인</button>
         </fieldset>
       </form>
     );
