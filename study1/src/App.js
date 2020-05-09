ad3f3bc5ec10b9f04bbfc68f79aa470759bead75
Leehaeun0 <a01050675111@gmail.com>
2020-05-09 19:20:48 +0900

    modify: change class to fuction

--- study1/src/App.js
+++ study1/src/App.js
@@ -1,86 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
 import style from './App.css';
+// eslint-disable-next-line import/no-unresolved
+import Button01 from './Button01';
+import { Input01, Input02 } from './Input01';
 
 const st = classNames.bind(style);
 
-class App extends Component {
-  idValue = '';
+let idValue = '';
 
-  passValue = '';
+let passValue = '';
 
-  inputChange = (e) => {
-    console.log(
-      `사용자 ${e.target.matches('#idInput') ? '아이디' : '비밀번호'} 입력중`
-    );
-  };
+const checkValue = ({ target }) => {
+  return [...target.firstElementChild.children].some((node) =>
+    node.nodeName !== 'INPUT' ? false : node.value === ''
+  );
+};
 
-  inputEnter = (e) => {
-    if (e.keyCode !== 13 || e.target.value.trim() === '') return;
-    console.log(
-      `사용자 ${e.target.matches('#idInput') ? '아이디' : '비밀번호'} '${
-        e.target.value
-      }' 입력`
-    );
-  };
-
-  inputAssign = (e) => {
-    if (e.target.matches('#idInput')) {
-      this.idValue = e.target.value;
-      return;
-    }
-    this.passValue = e.target.value;
-  };
-
-  checkValue = ({ target }) => {
-    return [...target.firstElementChild.children].some((node) =>
-      node.nodeName !== 'INPUT' ? false : node.value === ''
-    );
-  };
-
-  formSubmit = (e) => {
-    e.preventDefault();
-    if (this.checkValue(e)) {
-      console.log('입력란을 모두 입력해 주세요');
-      return;
-    }
-    console.log(`아이디: '${this.idValue}', 비밀번호: '${this.passValue}'`);
-    console.log('로그인 시도');
-    const [id, pass] = [...e.target.firstElementChild.children].filter(
-      (node) => node.nodeName === 'INPUT'
-    );
-    id.value = '';
-    pass.value = '';
-    this.idValue = '';
-    this.passValue = '';
-  };
-
-  render() {
-    return (
-      <form action="#" method="post" onSubmit={this.formSubmit}>
-        <fieldset>
-          <legend className={st('a11yHidden')}>로그인</legend>
-          <span className={st('label')}>ID</span>
-          <input
-            type="text"
-            id="idInput"
-            onChange={this.inputChange}
-            onBlur={this.inputAssign}
-            onKeyDown={this.inputEnter}
-          />
-          <span className={st('label')}>PASS</span>
-          <input
-            type="password"
-            id="passInput"
-            onChange={this.inputChange}
-            onBlur={this.inputAssign}
-            onKeyUp={this.inputEnter}
-          />
-          <button type="submit">로그인</button>
-        </fieldset>
-      </form>
-    );
+const formSubmit = (e) => {
+  if (e.keyCode === 13) return;
+  e.preventDefault();
+  if (checkValue(e)) {
+    console.log('입력란을 모두 입력해 주세요');
+    return;
   }
-}
+  console.log(`아이디: '${idValue}', 비밀번호: '${passValue}'`);
+  console.log('로그인 시도');
+  const [id, pass] = [...e.target.firstElementChild.children].filter(
+    (node) => node.nodeName === 'INPUT'
+  );
+  id.value = '';
+  pass.value = '';
+  idValue = '';
+  passValue = '';
+};
+
+const App = () => (
+  <form action="#" method="post" onSubmit={formSubmit}>
+    <fieldset>
+      <legend className={st('a11yHidden')}>로그인</legend>
+      <span className={st('label')}>ID</span>
+      <Input01 />
+
+      <span className={st('label')}>PASS</span>
+      <Input02 />
+
+      <Button01 />
+    </fieldset>
+  </form>
+);
 
 export default App;
+
+// import React, { Component } from 'react';
+// import ButtonChild01 from './ButtonChild01';
+// import ButtonChild02 from './ButtonChild02';
+
+// const AppStyle = {
+//   padding: 100,
+//   margin: 100,
+//   border: '1px solid #333'
+// };
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App" style={AppStyle}>
+//         <h2>{'부모 컴포넌트'}</h2>
+//         <ButtonChild01 title={'보라색 버튼'} color={'purple'} />
+//         <ButtonChild02 title={'빨간색 버튼'} color={'red'} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ study1/src/Button01.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Button01 = () => <button type="submit">로그인</button>;
+
+export default Button01;
