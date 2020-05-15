8bdf8cf45188d8c0d1593826fff79a2e545b70a0
Leehaeun0 <a01050675111@gmail.com>
2020-05-15 23:44:05 +0900

    add css

---
+++ phonebook/src/Component/App.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 className="title">Phone Book</h1>
+      <div className="book_wrap">
+        <div className="inputs_wrap">
+          <div className="div">
+            <span className="book_label">Name</span>
+            <input className="book_input" type="text" name="" id="" />
+          </div>
+          <div className="div">
+            <span className="book_label">Phone Number</span>
+            <input className="book_input" type="tel" name="" id="" />
+          </div>
+        </div>
+        <ul className="info_list">
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+          <li className="info">
+            이하은&nbsp;&nbsp;&nbsp;010wae50675111{' '}
+            <button type="button" className="info_btn">
+              X
+            </button>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default App;
