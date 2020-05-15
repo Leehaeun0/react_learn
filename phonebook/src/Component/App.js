05f2ed1113d64c70a01623969a466b7753d9c48e
Leehaeun0 <a01050675111@gmail.com>
2020-05-16 02:50:50 +0900

    add css

--- phonebook/src/Component/App.js
+++ phonebook/src/Component/App.js
@@ -3,89 +3,182 @@ import '../App.css';
 
 function App() {
   return (
-    <main>
-      <h1 className="title">Phone Book</h1>
-      <div className="book_wrap">
-        <div className="inputs_wrap">
-          <div className="div">
-            <span className="book_label">Name</span>
-            <input className="book_input" type="text" name="" id="" />
+    <>
+      <div className="page_bg"></div>
+      <main>
+        <h1 className="title">Phone Book</h1>
+        <div className="book_wrap">
+          <div className="inputs_wrap">
+            <div className="div">
+              <span className="book_label">
+                {/* <i className="fas fa-user" />&nbsp;  */}
+                Name
+              </span>
+              <input className="book_input" type="text" name="" id="" />
+            </div>
+            <div className="div">
+              <span className="book_label">
+                {/* <i className="fas fa-phone-alt" />&nbsp;  */}
+                Phone Number
+              </span>
+              <input className="book_input" type="tel" name="" id="" />
+            </div>
           </div>
-          <div className="div">
-            <span className="book_label">Phone Number</span>
-            <input className="book_input" type="tel" name="" id="" />
-          </div>
+          <ul className="info_list">
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+            <li className="info">
+              <span>이하은&nbsp;&nbsp;&nbsp;01050675111</span>
+              <button type="button" className="info_btn">
+                X
+              </button>
+            </li>
+          </ul>
         </div>
-        <ul className="info_list">
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;01050675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-          <li className="info">
-            이하은&nbsp;&nbsp;&nbsp;010wae50675111{' '}
-            <button type="button" className="info_btn">
-              X
-            </button>
-          </li>
-        </ul>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
